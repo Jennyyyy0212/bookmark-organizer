fc5835e8bfd725c85a32dca1d229f3e397eb19ed
Jenny Cheng <jenny14789632@gmail.com>
2024-11-16 17:48:35 -0800

    Complete background.js functionality

---
+++ background.js
@@ -0,0 +1,31 @@
+//triggeted when a profile that has this extension installed first starts up
+chrome.runtime.onInstalled.addListener(() => {
+    initialize();
+});
+  
+  chrome.runtime.onStartup.addListener(() => {
+    initialize();
+});
+
+function initialize () {
+    chrome.storage.local.get("setupComplete", (data) => {
+        if (!data.setupComplete) {
+          // If setup is not complete, run setup.js
+          import('./setup.js').then((module) => {
+            module.runSetup(() => {
+                // After setup, transition to bookmark.js
+                runBookmarkModule();
+            });
+          });
+        } else {
+            // Setup is complete, run bookmark.js
+            runBookmarkModule();
+        }
+      });
+}
+
+function runBookmarkModule() {
+    import('./bookmark.js').then((module) => {
+        module.addBook
+    });
+}
